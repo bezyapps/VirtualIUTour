5fdf802058f42175271648c890bcde002247e646
eric.bhatti <eric.bhatti@arpatech.com>
2015-11-19 12:45:26 +0500

    Added Public IP

--- app/src/main/java/com/rea/learn/Settings.java
+++ app/src/main/java/com/rea/learn/Settings.java
@@ -20,7 +20,7 @@ public class Settings extends AppCompatActivity {
     NumberPicker numberPickerFrame;
     public static final String SKIP_RATE = "skipRate";
     public static final String IP_ADDRESS = "ipAddress";
-    public static final String[] IPs = {"http://192.168.11.252:8080/StrutsMavenProject/image.json", "http://192.168.1.178:8080/image.json", "http://192.168.:8080/image.json"};
+    public static final String[] IPs = {"http://192.168.11.252:8080/StrutsMavenProject/image.json", "http://110.93.196.115:8080/StrutsMavenProject/image.json","http://192.168.1.178:8080/image.json", "http://192.168.:8080/image.json"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
